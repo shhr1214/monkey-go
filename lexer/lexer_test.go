ae2f71b1b328fca495e093f6626e0c4fc87b8053
shhr1214 <souhei0079@gmail.com>
2018-08-14 01:17:47 +0900

    add: lexer のテストを追加

---
+++ lexer/lexer_test.go
@@ -0,0 +1,36 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/shhr1214/monkey-go/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+
+	tests := []struct {
+		expectedType    token.Type
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("test[%d] - tokentype wrong. expected=%q, got=%q")
+		}
+	}
+}
