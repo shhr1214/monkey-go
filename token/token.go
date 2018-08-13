f06a5c599b64fa47f0f56bb4c7c7c64c78928cf6
shhr1214 <souhei0079@gmail.com>
2018-08-13 23:04:07 +0900

    add: トークンの定義を追加

---
+++ token/token.go
@@ -0,0 +1,37 @@
+package token
+
+// 定数
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// 識別子 + リテラル
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1234567890
+
+	// 演算子
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// デリミタ
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// キーワード
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+// Type はトークンの種類を表す。
+type Type string
+
+// Token は字句解析器が出力するトークン。
+type Token struct {
+	Type Type
+	Literal string
+}
