377a4852c33efea7856f42459921c98254d471bd
shhr1214 <souhei0079@gmail.com>
2018-09-03 00:33:56 +0900

    let の解析用

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "github.com/shhr1214/monkey-go/token"
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -12,4 +14,36 @@ type Statement interface {
 type Expression interface {
 	Node
 	expressionNode()
+}
+
+type Program struct {
+	Statement []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statement) > 0 {
+		return p.Statement[0].TokenLiteral()
+	}
+	return ""
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLieteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
 }
