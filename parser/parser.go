8d26d01d65f788998f24d9ac1e6e54f275b8765c
shhr1214 <souhei0079@gmail.com>
2018-09-18 01:41:19 +0900

    add: return の構文解析
    とりあえずの骨組み。
    まだ内容を見ているわけではない。

--- parser/parser.go
+++ parser/parser.go
@@ -51,6 +51,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -65,6 +67,19 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 
 	if !p.expectPeek(token.ASSIGN) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// TODO: セミコロンがあるまで式をとばす
+	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
