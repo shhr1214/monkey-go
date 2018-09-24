87b7c23ded1249f530768d9df6c20d64577cc4a3
shhr1214 <souhei0079@gmail.com>
2018-09-24 23:35:57 +0900

    fix: test error message
    なんか、Let のテストが落ちている原因てきなものが表示された気がした。

--- parser/parser.go
+++ parser/parser.go
@@ -132,9 +132,15 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 	return stmt
 }
 
-func (p *Parser) parseExpression(precedence int) ast.Expression {
+func (p *Parser) noPrefixParseFnError(t token.Type) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) parseExpression(preceence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
 	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
 	leftExp := prefix()
